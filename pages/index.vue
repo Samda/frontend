5f3911bd65a12fd5ddde48877ccb2d4ef4288ac7
samda <muy.samda13@gmail.com>
2020-07-01 14:49:43 +0700

    add font-awesomes

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <section class="hero is-light is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-4 is-offset-4">
+              <div class="field">
+                <!-- <label class="label">Email</label> -->
+                <div class="control has-icons-left has-icons-right">
+                  <input class="input is-primary" type="email" placeholder="Email">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-envelope fa-xs"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <!-- <i class="fas fa-check fa-xs"></i> -->
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <!-- <label class="label">Password</label> -->
+                <div class="control has-icons-left has-icons-right">
+                  <input class="input is-primary" type="email" placeholder="Password">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-lock fa-xs"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <!-- <i class="fas fa-check fa-xs"></i> -->
+                  </span>
+                </div>
+              </div>
+              <div class="buttons">
+                <button class="button is-fullwidth is-success">LOGIN</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
