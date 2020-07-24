8e15e0547bf955f8b742d8054c98ddde90c1a8db
samda <muy.samda13@gmail.com>
2020-07-24 09:06:33 +0700

    fix nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
     title: 'KH LAND Co.,ltd.',
     meta: [
       { charset: 'utf-8' },
-      {"http-equiv"": "X-UA-Compatible", content: "IE=edge"},
+      {"http-equiv": "X-UA-Compatible", content: "IE=edge"},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
