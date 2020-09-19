da0a9f4973ff63e94a00e5955703e393efc6ae48
samda <muy.samda13@gmail.com>
2020-09-19 13:09:00 +0700

    add color

--- components/clients/FooterMain.vue
+++ components/clients/FooterMain.vue
@@ -1,20 +1,23 @@
 <template lang="pug">
-v-footer(, dark, padless)
+v-footer.main-footer(, dark, padless, color="#6b4912")
   v-container.ma-0.pa-0(fluid)
-    v-row.pb-10(align="top", no-gutters, style="background-color: #3B3B3B")
+    v-row.pb-10(align="top", no-gutters, style="background-color: #78a152")
       v-col(cols="3")
-        v-card.white--text(style="background-color: #3B3B3B", flat)
-          v-card-title.justify-center KH LAND
+        v-card.brown--text.text--darken-2(style="background-color: #78a152", flat)
+          v-card-title.justify-center(color="#6b4912") KH LAND
       v-col(cols="9")
         v-row(no-gutters)
           v-col(v-for="title in itemTitle", :key="title")
-            v-card-title(style="background-color: #3B3B3B") {{ title.text }}
+            v-card-title.brown--text.text--darken-2(style="background-color: #78a152") {{ title.text }}
             v-list.pl-4(
-              style="background-color: #3B3B3B",
+              style="background-color: #78a152",
               v-for="(content, index) in itemContent",
               :key="index"
             )
-              v-list-item-title {{ content.text }}
+              v-list-item-title.brown--text.text--darken-2 {{ content.text }}
+    v-row(style="background-color: #628740")
+      v-col(cols="12")
+        .caption Copyright © 2020 by KHLAND CO,.LTD. All Right Reserved.
 </template>
 
 <script>
@@ -38,3 +41,9 @@ export default {
   },
 };
 </script>
+
+<style type="scss" scoped>
+  .main-footer {
+    color: #6b4912;
+  }
+</style>
