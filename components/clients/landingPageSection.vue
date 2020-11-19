2f1ab98c67cc4ce94188737f38665f2ecc7a7f89
samda <muy.samda13@gmail.com>
2020-11-19 07:03:42 +0700

    add image

--- components/clients/landingPageSection.vue
+++ components/clients/landingPageSection.vue
@@ -30,14 +30,13 @@ div
         v-img(
           max-height="250",
           contain,
-          src="https://scontent.fpnh1-2.fna.fbcdn.net/v/t1.0-9/117893929_1802415019924729_149439977059688306_o.jpg?_nc_cat=111&_nc_sid=730e14&_nc_ohc=kd6fPa7JFIIAX-mWGl1&_nc_ht=scontent.fpnh1-2.fna&oh=71dd99e25253c3fcedede97989ae70ef&oe=5F94A453"
+          :src="require('~/assets/images/featureImage1.jpg')"
         )
     v-divider
     v-row(justify="space-around", align="center", height="500px")
       v-col.pa-10(cols="12", md="6")
-        //-v-img(src="https://i.pinimg.com/originals/ed/11/c3/ed11c33ed70d7635c615e6dc0605e28b.jpg")
         v-img(
-          src="https://scontent.fpnh1-2.fna.fbcdn.net/v/t1.0-9/117716105_1802415089924722_3006423376808006100_o.jpg?_nc_cat=111&_nc_sid=730e14&_nc_ohc=xSJ8KQ3NDbwAX8467My&_nc_ht=scontent.fpnh1-2.fna&oh=37f05b376c75c2bbac246734d3c042af&oe=5F9578A0"
+          :src="require('~/assets/images/featureImage2.jpg')"
         )
       v-col(cols="12", md="6")
         div
@@ -52,7 +51,7 @@ div
           p.body-1.text-center.brown--text.text--darken-4 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
       v-col.pa-10(cols="12", md="6")
         v-img(
-          src="https://raw.githubusercontent.com/Samda/khland-assets/master/living%20room%204.jpg"
+          :src="require('~/assets/images/featureImage3.jpg')"
         )
     v-divider
 
@@ -62,7 +61,8 @@ div
         v-card.mx-auto(max-width="400")
           v-img.white--text.align-end(
             height="200px",
-            src="https://scontent.fpnh1-2.fna.fbcdn.net/v/t1.0-9/117972144_1802415126591385_8918528493799312042_o.jpg?_nc_cat=108&_nc_sid=730e14&_nc_ohc=VnQ5SthFxKgAX8CyK6-&_nc_ht=scontent.fpnh1-2.fna&oh=479cddd2b7b2cfa29bfc00b78faa60fb&oe=5F962CE6"
+            src="./static/"
+            :src="require('~/assets/images/house.jpg')"
           )
             v-card-title Twin Villa
           v-card-subtitle.pb-0 Two Bedroom
@@ -78,7 +78,7 @@ div
         v-img(
           gradient="rgba(0,0,0,.6), rgba(0,0,0,.6) ",
           height="450px",
-          src="https://scontent.fpnh1-1.fna.fbcdn.net/v/t1.0-9/117879720_1802414966591401_3975173550225361756_o.jpg?_nc_cat=105&_nc_sid=730e14&_nc_eui2=AeEAV2Utzzv-oLSnwD5JEX3qwMaW0-45qmfAxpbT7jmqZwcgkYBp3dxHhyGhfB3k8W0&_nc_ohc=J-dr_L3EApkAX9bPErf&_nc_ht=scontent.fpnh1-1.fna&oh=86f3958e691bdbab90412e3152202e6e&oe=5F8B99F0")
+          :src="require('~/assets/images/featureImage4.jpg')")
           v-row
             v-col(cols="12")
               .text-center.mt-15
@@ -104,6 +104,9 @@ div
 </template>
 
 <script>
+import image1 from '~/assets/images/landingImage1.jpg'
+import image2 from '~/assets/images/landingImage2.jpg'
+import image3 from '~/assets/images/landingImage3.jpg'
 export default {
   data() {
     return {
@@ -114,21 +117,21 @@ export default {
           content: 'ដោយការជ្រើសរើសនូវលំនៅដ្ឋាន ដ៏សែនសមរម្យមួយសំរាប់រូបអ្នកនឹងគ្រួសារអ្នក',
           text: 'primary',
           code: '#78a152',
-          image: 'https://scontent.fpnh1-2.fna.fbcdn.net/v/t1.0-9/117783770_1802414876591410_3603053929977972203_o.jpg?_nc_cat=111&_nc_sid=730e14&_nc_ohc=Dqy3C1A3DAoAX8WQqUg&_nc_ht=scontent.fpnh1-2.fna&oh=c793360342c558be9e8f398a07a72a4d&oe=5F95D15F'
+          image: image1
         },
         {
           title: 'ដោយជ្រើរើសយកផ្ទះណាមួយ',
           content: 'ដែលអ្នកពេញចិត្ត យើងមានផ្ទះជាច្រើនម៉ូតសំរាប់អ្នកជ្រើសរើស ដើម្បីបំពេញតម្រូវការរបស់អ្នក',
           text: 'secondary',
           code: '#59482c',
-          image: 'https://scontent.fpnh1-1.fna.fbcdn.net/v/t1.0-9/117610617_1802414863258078_30441688992698031_o.jpg?_nc_cat=105&_nc_sid=730e14&_nc_ohc=BO8zTUJZxNUAX-OfdMy&_nc_ht=scontent.fpnh1-1.fna&oh=5f29fb77de2fc28ff36625daa8ba2cb0&oe=5F977F62'
+          image: image2
         },
         {
           title: 'ហើយអាចកែតម្រូវតៅតាមចំនូលចិត្តរបស់អ្នក',
           content: 'អ្នកអាចកែប្រែ ផ្លាស់ប្តូ តុបតែង នឹង ជ្រើរើសសំភារៈ នឹង ឧបករណ៍ផ្សេងៗ',
           text: 'primary',
           code: '#59482c',
-          image: 'https://scontent.fpnh1-2.fna.fbcdn.net/v/t1.0-9/117716105_1802415089924722_3006423376808006100_o.jpg?_nc_cat=111&_nc_sid=730e14&_nc_ohc=xSJ8KQ3NDbwAX8467My&_nc_ht=scontent.fpnh1-2.fna&oh=37f05b376c75c2bbac246734d3c042af&oe=5F9578A0'
+          image: image3
         }
       ],
       contents: [
