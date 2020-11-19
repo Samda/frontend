e7686833fe7553ba68b49a742a6091ccdcc60708
samda <muy.samda13@gmail.com>
2020-11-19 07:45:27 +0700

    fix build

--- dist/_nuxt/b2ad3d4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{636:function(n,t,e){"use strict";e.r(t);var o={layout:"configuration"},c=e(31),component=Object(c.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("Finish")])}),[],!1,null,"795ab767",null);t.default=component.exports}}]);
